8b0a1bbde9d6bbd7724760a1b67bb715d19b6786
houari <houari@as.com>
2023-05-12 20:14:36 +0100

    first commit

---
+++ app/routers/user.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException, status, Depends, APIRouter
+from .. import models, schemas, utils
+from sqlalchemy.orm import Session
+from ..database import get_db
+
+
+router=APIRouter(
+    prefix="/users"
+)
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def users(new_user: schemas.UserCreate, db: Session=Depends(get_db)):
+    
+    hashed_password=utils.hash(new_user.password)
+    new_user.password=hashed_password
+    new_user=models.User(**new_user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.UserOut)
+def get_user(id: int, db: Session=Depends(get_db)):
+    user=db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="not found")
+    return user
